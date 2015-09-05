f84974ac3b6ed315bf58f19da3e786fad552b3a3
sunleialbert <whqwill@gmail.com>
2015-09-06 00:26:57 +0200

    d

--- src/main/java/W2VsparkMllib.java
+++ src/main/java/W2VsparkMllib.java
@@ -1,7 +1,12 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.apache.spark.mllib.feature.Word2Vec;
+import org.apache.spark.mllib.feature.Word2VecModel;
 
 /**
  * Created by will on 05/09/15.
@@ -10,9 +15,20 @@ public class W2VsparkMllib {
     private static Logger log = LoggerFactory.getLogger(W2VsparkMllib.class);
 
     public static void main(String[] args) throws Exception {
-        long begintime = System.currentTimeMillis();
+        /*long begintime = System.currentTimeMillis();
         SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("sparktest");
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
+        String dataPath = new ClassPathResource("raw_sentences.txt").getFile().getAbsolutePath();
+        JavaRDD<String> corpus = sc.textFile(dataPath);
+        Word2Vec vec = new Word2Vec();
+        String a = "afs dfsa ssd";
 
+        class F implements Function<String, String[]> {
+            public String[] call(String s) { return new Iterable<String>(s.split(" ")); }
+        }
+
+        Word2VecModel model = vec.fit(corpus.map(new F()));
+
+        model.findSynonyms("day",10);*/
     }
 }
